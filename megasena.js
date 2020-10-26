1ea0c7b9f94043862e98d7a71a420b0223c99b86
Daniel Leon <daniel.radialista@gmail.com>
2020-10-26 17:01:04 -0300

    gera 6 numeros aleatorios

---
+++ megasena.js
@@ -0,0 +1,11 @@
+
+
+let firstNumber = Math.ceil(Math.random() * 60);
+let secoundNumber = Math.ceil(Math.random() * 60);
+let thirdNumber = Math.ceil(Math.random() * 60);
+let fourthNumber = Math.ceil(Math.random() * 60);
+let fifthNumber = Math.ceil(Math.random() * 60);
+let sixtNumber = Math.ceil(Math.random() * 60);
+let megaSenaNumbers = [firstNumber, secoundNumber, thirdNumber, fourthNumber, fifthNumber, sixtNumber];
+
+console.log(megaSenaNumbers);
